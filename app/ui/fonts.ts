d3a45297d2a248350a2c878778ab29d70bd66291
Jay Flynn <jay_flynn@live.com>
2024-02-29 17:18:24 +0000

    first commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
-import { Roboto, Rubik } from 'next/font/google';
+import { Roboto, Rubik, Poppins } from 'next/font/google';
 
 export const robotto = Roboto({
   weight: ['400'],
   subsets: ['latin'],
 });
 export const rubik = Rubik({ weight: ['400'], subsets: ['latin'] });
+
+export const poppins = Poppins({ weight: ['400'], subsets: ['latin'] });
